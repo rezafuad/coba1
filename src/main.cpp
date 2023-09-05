0b9e1c9f523059abb7f090a1bfcd73e4a8e4a019
Reza Fuad Rachmadi <fuad@its.ac.id>
2023-09-05 14:21:53 +0700

    Branch baru

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,14 @@
 
 int main(int argc, char**argv)
 {
+    float l;
+
+    std::cout << "Masukkan panjang segiempat : ";
+    std::cin >> l;
+
     std::cout << "Hello world!" << std::endl;
 
+    std::cout << "Luas segiempat adalah " << l*l << std::endl;
+    
     return EXIT_SUCCESS;
 }
